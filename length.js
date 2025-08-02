109da0f7445fa2313c9712c64ac6a7a883aefe6b
Navid Noman <navidnoman348n@gmail.com>
2025-08-03 01:51:11 +0600

    commit add new arry length.js add and start new somthing

--- length.js
+++ length.js
@@ -1,2 +1,12 @@
-const numbers =[1,5,7,88,76,54,33,22,88,76,54,33,22];
-console.log(numbers.length);
+// const numbers =[1,5,7,88,76,54,33,22,88,76,54,33,22];
+// console.log(numbers.length);
+
+
+const num =[54,33,22,88,76,54]
+
+console.log(num);
+console.log(num[5])
+// console.log(num[5])
+
+const fourth = num[5]
+console.log(fourth)
