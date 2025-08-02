b37cd21f5725c537902a062082b8c6a58e0227c1
Navid Noman <navidnoman348n@gmail.com>
2025-08-02 18:33:08 +0600

    my 2nd ripo to arrey

---
+++ array.js
@@ -0,0 +1,10 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 14;
+// const num4 = 26;
+// const num5 = 30;
+
+const numbers = [12,22,24]
+const friend = ['navid', 'noman', 'jaman', 'kaman']
+const isPass = [true, false, true,]
+const mixedArray=['project', 21, true,]
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers =[1,5,7,88,76,54,33,22,88,76,54,33,22];
+console.log(numbers.length);
